19ad3478aac333d788c73b108e8a9ba5fbd671c2
Gon Solo <gonsolo@gmail.com>
2023-12-08 16:21:33 +0100

    set_byte.

--- verilog/dv/cocotb/pepe/pepe.py
+++ verilog/dv/cocotb/pepe/pepe.py
@@ -17,73 +17,27 @@
 from caravel_cocotb.caravel_interfaces import *
 import cocotb
 
+async def set_byte(env, input_set, select1, select2, bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7):
+    env.drive_gpio_in(28, input_set)
+    env.drive_gpio_in(27, select1)
+    env.drive_gpio_in(26, select2)
+    env.drive_gpio_in(25, bit0)
+    env.drive_gpio_in(24, bit1)
+    env.drive_gpio_in(23, bit2)
+    env.drive_gpio_in(22, bit3)
+    env.drive_gpio_in(21, bit4)
+    env.drive_gpio_in(20, bit5)
+    env.drive_gpio_in(19, bit6)
+    env.drive_gpio_in(18, bit7)
+    await cocotb.triggers.ClockCycles(env.clk, 2)
+
 async def pepe_test(env):
 
     # 32 bit float -33.f as hex bits: C2040000
-
-    # Set first byte: C2
-    env.drive_gpio_in(28, 1)
-    # 27-26 select one of four bytes
-    env.drive_gpio_in(27, 1)
-    env.drive_gpio_in(26, 1)
-    # 25-18: byte to set: C2
-    env.drive_gpio_in(25, 1)
-    env.drive_gpio_in(24, 1)
-    env.drive_gpio_in(23, 0)
-    env.drive_gpio_in(22, 0)
-    env.drive_gpio_in(21, 0)
-    env.drive_gpio_in(20, 0)
-    env.drive_gpio_in(19, 1)
-    env.drive_gpio_in(18, 0)
-    await cocotb.triggers.ClockCycles(env.clk, 2)
-
-    # Set second byte: 04
-    env.drive_gpio_in(28, 1)
-    # 27-26 select one of four bytes
-    env.drive_gpio_in(27, 1)
-    env.drive_gpio_in(26, 0)
-    # 25-18: byte to set: C2
-    env.drive_gpio_in(25, 0)
-    env.drive_gpio_in(24, 0)
-    env.drive_gpio_in(23, 0)
-    env.drive_gpio_in(22, 0)
-    env.drive_gpio_in(21, 0)
-    env.drive_gpio_in(20, 1)
-    env.drive_gpio_in(19, 0)
-    env.drive_gpio_in(18, 0)
-    await cocotb.triggers.ClockCycles(env.clk, 2)
-
-    # Set third byte: 00
-    env.drive_gpio_in(28, 1)
-    # 27-26 select one of four bytes
-    env.drive_gpio_in(27, 0)
-    env.drive_gpio_in(26, 1)
-    # 25-18: byte to set: C2
-    env.drive_gpio_in(25, 0)
-    env.drive_gpio_in(24, 0)
-    env.drive_gpio_in(23, 0)
-    env.drive_gpio_in(22, 0)
-    env.drive_gpio_in(21, 0)
-    env.drive_gpio_in(20, 0)
-    env.drive_gpio_in(19, 0)
-    env.drive_gpio_in(18, 0)
-    await cocotb.triggers.ClockCycles(env.clk, 2)
-
-    # Set forth byte: 00
-    env.drive_gpio_in(28, 1)
-    # 27-26 select one of four bytes
-    env.drive_gpio_in(27, 0)
-    env.drive_gpio_in(26, 0)
-    # 25-18: byte to set: C2
-    env.drive_gpio_in(25, 0)
-    env.drive_gpio_in(24, 0)
-    env.drive_gpio_in(23, 0)
-    env.drive_gpio_in(22, 0)
-    env.drive_gpio_in(21, 0)
-    env.drive_gpio_in(20, 0)
-    env.drive_gpio_in(19, 0)
-    env.drive_gpio_in(18, 0)
-    await cocotb.triggers.ClockCycles(env.clk, 2)
+    await set_byte(env, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0) # C2
+    await set_byte(env, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0) # 04
+    await set_byte(env, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0) # 00
+    await set_byte(env, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) # 00
 
     # Result in hex: c128114f
     env.drive_gpio_in(28, 0)
